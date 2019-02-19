7efe4bfd15f2812b3f220b4c1d484226b3df2faa
Matteo Veroni <matver87@gmail.com>
2019-02-20 00:56:31 +0100

    reinitialized files

---
+++ src/main/java/com/matteoveroni/randomgenerator/RandIntegerWithoutRepetitionGenerator.java
@@ -0,0 +1,59 @@
+package com.matteoveroni.randomgenerator;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Matteo Veroni
+ *
+ * Returns a unique random integer in a range delimited by an upper and lower
+ * value without the possibility of repetition.
+ *
+ * Example: domain(1-4) - {1,2,3,4} ===> nextInt() = 3 -> nextInt() = 4 ->
+ * nextInt() = 1 -> nextInt() = 2 -> nextInt() = Exception
+ */
+public class RandIntegerWithoutRepetitionGenerator {
+
+    private final int maxBound;
+    private final int minBound;
+    private final List<Integer> numbers = new LinkedList<>();
+
+    public RandIntegerWithoutRepetitionGenerator(int minBound, int maxBound) throws IllegalArgumentException {
+        if (minBound >= maxBound) {
+            throw new IllegalArgumentException("minRange must be smaller than maxRange");
+        }
+        this.maxBound = maxBound;
+        this.minBound = minBound;
+        resetNumberRepetionSystem();
+    }
+
+    /**
+     * Method which returns a random integer without repetition
+     * @return A random integer without repetition
+     * @throws Exception if is not possible to extract any other integer because of repetition an exception is thrown.
+     */
+    public final int nextInt() throws Exception {
+        if (!numbers.isEmpty()) {
+            return numbers.remove(0);
+        } else {
+            throw new Exception("No more possible extractions");
+        }
+    }
+
+    /**
+     * Method which resets the logical model behind the number repetition system
+     */
+    public final void resetNumberRepetionSystem() {
+        initNumbers();
+        Collections.shuffle(numbers);
+    }
+
+    private void initNumbers() {
+        for (int i = minBound; i <= maxBound; i++) {
+            numbers.add(i);
+        }
+    }
+
+}
---
+++ src/test/java/com/matteoveroni/randomgenerator/RandIntegerWithoutRepetitionGeneratorTest.java
@@ -0,0 +1,73 @@
+package com.matteoveroni.randomgenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class RandIntegerWithoutRepetitionGeneratorTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void if0ThrowIllegalArgument() {
+        new RandIntegerWithoutRepetitionGenerator(0, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ifNegativeThrowIllegalArgument() {
+        new RandIntegerWithoutRepetitionGenerator(0, -5);
+    }
+
+    @Test
+    public void ifBetween0And1Extract0Or1() throws Exception {
+        int min = 0;
+        int max = 1;
+        RandIntegerWithoutRepetitionGenerator rand = new RandIntegerWithoutRepetitionGenerator(min, max);
+
+        int firstExtraction = rand.nextInt();
+        int secondExtraction = rand.nextInt();
+
+        assertTrue(firstExtraction >= min && firstExtraction <= max);
+        assertTrue(secondExtraction >= min && secondExtraction <= max);
+
+        // If first and secondExtraction are the opposite number between 0 and 1 their module should always be 1
+        assertTrue((firstExtraction ^ secondExtraction) == 1);
+    }
+
+    @Test
+    public void ifBetween2And15ExtractOnlyCompatibleNumbers() {
+        int min = 2;
+        int max = 15;
+        RandIntegerWithoutRepetitionGenerator rand = new RandIntegerWithoutRepetitionGenerator(min, max);
+
+        List<Integer> extractedNumbers = new ArrayList<>();
+        List<Integer> listOfExpectedNumbers = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            listOfExpectedNumbers.add(i);
+            try {
+                Integer extractNumber = rand.nextInt();
+                extractedNumbers.add(extractNumber);
+                assertTrue(extractNumber >= min);
+                assertTrue(extractNumber <= max);
+            } catch (Exception ex) {
+            }
+        }
+
+        for (Integer number : extractedNumbers) {
+            listOfExpectedNumbers.remove(number);
+        }
+
+        assertTrue(listOfExpectedNumbers.isEmpty());
+
+        Exception exOccurred = null;
+        try {
+            rand.nextInt();
+        } catch (Exception ex) {
+            exOccurred = ex;
+        } finally {
+            assertNotNull(exOccurred);
+            assertEquals("No more possible extractions", exOccurred.getMessage());
+        }
+    }
+}
